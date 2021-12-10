e1c47c67c25e938704c85c29f19e594223aafe87
sarahwaity <76963375+sarahwaity@users.noreply.github.com>
2021-12-10 15:36:29 -0800

    rate extr error

--- rateExtrapolation/__init__.py
+++ rateExtrapolation/__init__.py
@@ -1,4 +1,4 @@
 from k_folds_data_splitter import k_folds_data_splitter
 from parameter_estimation_fitness_evaluator import parameter_estimation_fitness_evaluator
-from rate_Extrapolation import rateExtrapolation
+from rateextrapolation import rateextrapolation
 from sbstoat_model_fitting_to_folds import sbstoat_model_fitting_to_folds
