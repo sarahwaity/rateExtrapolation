ddb8f1e56dc60111c859685dd633bd73cc81cb0a
sarahwaity <76963375+sarahwaity@users.noreply.github.com>
2021-12-10 15:25:28 -0800

    pep8

--- rateExtrapolation/tests/test_k_folds_data_splitter.py
+++ rateExtrapolation/tests/test_k_folds_data_splitter.py
@@ -3,7 +3,7 @@ This file will be used to test the K_folds_data_splitter.py file.
 """
 
 # Import cleaning and splitting
-from K_folds_data_splitter import K_folds_data_splitter
+from k_folds_data_splitter import k_folds_data_splitter
 
 # Importing libraries for property tests
 import math
@@ -24,7 +24,7 @@ def test_k_folds_data_splitter_1():
     Test to determine if number of columns matches the input number of folds
     '''
     folds = 4
-    df = K_folds_data_splitter(data, folds)
+    df = k_folds_data_splitter(data, folds)
     folds_num = len(df.columns)
     assert folds_num == folds, (
         "Number of columns in consistent with number of folds, there may be an assignment error")
@@ -40,7 +40,7 @@ def test_k_folds_data_splitter_2():
     Test to determine if rows for test and train data are returned
     '''
     folds = 4
-    df = K_folds_data_splitter(data,folds)
+    df = k_folds_data_splitter(data,folds)
     rows_num = len(df)
     assert rows_num == 2, (
         "Number of rows is inconsistent with Test/Train, function assignment has failed")
@@ -55,7 +55,7 @@ def test_k_folds_data_splitter_3():
     Test to determine if number of samples in test set is less than sample number of train set
     '''
     folds = 4
-    df = K_folds_data_splitter(data,folds)
+    df = k_folds_data_splitter(data,folds)
     test_set_length = len(df.loc['Test'][0])
     train_set_length = len(df.loc['Train'][0])
     
