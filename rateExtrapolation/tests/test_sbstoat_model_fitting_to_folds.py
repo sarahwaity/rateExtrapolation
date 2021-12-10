4d1fc5d1dee78b23b578ff4356b8aeff69650e13
sarahwaity <76963375+sarahwaity@users.noreply.github.com>
2021-12-10 15:28:10 -0800

    pep8

--- rateExtrapolation/tests/test_sbstoat_model_fitting_to_folds.py
+++ rateExtrapolation/tests/test_sbstoat_model_fitting_to_folds.py
@@ -3,8 +3,8 @@ This file will be used to test the SBstoat_model_fitting_to_folds.py file.
 """
 
 # Import cleaning and splitting
-from K_folds_data_splitter import K_folds_data_splitter
-from SBstoat_model_fitting_to_folds import SBstoat_model_fitting_to_folds
+from k_folds_data_splitter import k_folds_data_splitter
+from sbstoat_model_fitting_to_folds import sbstoat_model_fitting_to_folds
 
 # Importing libraries for property tests
 import math
@@ -17,7 +17,7 @@ import SBstoat as SB
 
 data = pd.read_csv('data/test_function_data.csv', index_col=0)
 
-df = K_folds_data_splitter(data, folds = 4)
+df = k_folds_data_splitter(data, folds = 4)
 
 antimony = """
     S1 -> S2; k1*S1;
@@ -37,11 +37,11 @@ for rate in rates:
 # In[3]:
 
 
-def test_SBstoat_model_fitting_to_folds_1():
+def test_sbstoat_model_fitting_to_folds_1():
     '''
     Test to determine if number of rate estimation columns matches the input number of rate
     '''
-    sb_model_fit = SBstoat_model_fitting_to_folds(antimony, rates, df,pathway_parameters)
+    sb_model_fit = sbstoat_model_fitting_to_folds(antimony, rates, df,pathway_parameters)
     assert (len(sb_model_fit.columns)-4) == len(rates), (
         "Number of estimated rates does not equal number of designated rates")
     return
@@ -50,11 +50,11 @@ def test_SBstoat_model_fitting_to_folds_1():
 # In[4]:
 
 
-def test_SBstoat_model_fitting_to_folds_2():
+def test_sbstoat_model_fitting_to_folds_2():
     '''
     Test to determine estimation was run over the number of folds provided
     '''
-    sb_model_fit = SBstoat_model_fitting_to_folds(antimony, rates, df,pathway_parameters)
+    sb_model_fit = sbstoat_model_fitting_to_folds(antimony, rates, df,pathway_parameters)
     assert len(sb_model_fit) == len(df.T), (
         "Number of fold estimations does not equal number of folds given")
     return
@@ -63,11 +63,11 @@ def test_SBstoat_model_fitting_to_folds_2():
 # In[5]:
 
 
-def test_SBstoat_model_fitting_to_folds_3():
+def test_sbstoat_model_fitting_to_folds_3():
     '''
     Test to determine that the column names match rates list, important in downstream processing
     '''
-    sb_model_fit = SBstoat_model_fitting_to_folds(antimony, rates, df,pathway_parameters)
+    sb_model_fit = sbstoat_model_fitting_to_folds(antimony, rates, df,pathway_parameters)
 
     assert list(sb_model_fit.columns[0:len(rates)]) == rates, (
         "rates returned do not match rates provided, check case and antimony string")
