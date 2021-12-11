eddb054b279156d0367dde6d4595d10acdfcaa52
sarahwaity <76963375+sarahwaity@users.noreply.github.com>
2021-12-10 19:58:53 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
 	name = 'rateExtrapolation',
-	version = '1.0.0',
+	version = '1.0.1',
 	description = 'Package returns estimations for model rates.',
 	long_description = 'This package simplifies reate determination from experimental data using SBstoat and Tellurium.',
 	author = 'Sarah Wait',
 	author_email = 'swait@uw.edu',
 	url = 'https://github.com/sarahwaity/rateExtrapolation.git',
 	keywords = 'tellurium rate extrapolation',
-	install_requires = ['matplotlib == 3.3.4','numpy == 1.20.2','pandas == 1.2.4','SBstoat == 1.161', 'tellurium == 2.2.0'],
-	packages = find_packages(exclude = ('docs', 'tests*', 'testing*'),
 	license = 'MIT',
-	entry_points = {
-		'console_scripts' : [
-			'rateExtrapolation = rateExtrapolation:rate_Extrapolation']})
+	install_requires = ['matplotlib == 3.3.4','numpy == 1.19.3','pandas == 1.2.4','SBstoat == 1.161', 'tellurium == 2.2.0'],
+	packages = find_packages(exclude = ('docs', 'tests*', 'testing*')))
